aab5cee23f596f63f3a3333a28d76ae33f7b5f9f
tatiane <seu emailtatianetenorio.1990@gmail.comgit initgitgit initgit config --global user.email seu>
2024-08-12 21:28:53 -0300

    aula 03 CSS

---
+++ teste facul.cpp
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+struct
+    registroAluno {
+	    int id;
+		char nome[50];
+    };
+    registroAluno aluno;
+    
+main() {
+	scanf("%d", &aluno.id);
+	scanf("%s", aluno.nome);
+	scanf("%s",aluno_endereco);
+	
+	printf("id do aluno ...:%i\n", aluno.id);
+	printf("nome do aluno...:%s\n", aluno.nome);
+	printf("endereço do aluno..:%s\n", aluno.endereco);
+}    
